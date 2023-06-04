83621077c541a3aef7cdb5828f5d03da90b66232
Chairat634230061 <634230061@webmail.npru.ac.th>
2023-06-04 17:34:03 +0700

    first commit

--- page/user/user.php
+++ page/user/user.php
@@ -4,6 +4,7 @@
         <div class= nav_container>
             <a href="uhot.php"><img src="./img/logo.png" class="logonav"></a>
             <a href="logout.php" class="button-30">ออกจากระบบ</a>
+        </div>
         <div class = "conta">
         
             
